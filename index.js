595868ca9f486cf126c1aed9c5ae8181cbb65cd1
Ebuka Mbata <rockymmpr@gmail.com>
2020-06-03 22:24:20 +0100

    refactoring code (node dilemmas)

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const path = require('path'); //built-in module like fs
 const app = express(); // initialising the express function and its features - https://npmjs.com/package/express
 const bodyParser = require('body-parser'); //npm i body-parser
 require('dotenv').config();
+const PORT = process.env.PORT
 
 const openWeatherMap = require('./lib/WeatherMap') // multiple imports
 const openHarryPotter = require('./lib/HarryPotter')
 const openNASA = require('./lib/NASA')
-const openPokemon = require('./lib/Pokemon')
-const openSuperHero = require('./lib/SuperHero')
+const Pokemon = require('./lib/Pokemon') // single imports
+const SuperHero = require('./lib/SuperHero')
 
 app.use(bodyParser.urlencoded({extended: false})); //ignore data types and make everything a string
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //
 
 app.engine('.hbs', handleBars({
     defaultLayout: 'layout',
@@ -23,6 +24,10 @@ app.engine('.hbs', handleBars({
 app.set('view engine', '.hbs')
 
 app.get('/', async (req, res) => {
+    res.render('index')
+});
+
+app.get('/weathermap', async (req, res) => {
     let weatherData = await openWeatherMap.getWeather();
     console.log(weatherData);
     let temp = weatherData.main.temp;
@@ -37,7 +42,7 @@ app.get('/', async (req, res) => {
     //res.send(weatherData);
     //let data = await openWeatherMap.anotherFunction();
     //console.log(data);
-    res.render('index', { temp, name, country, main, description, longitude, latitude, base, clouds })
+    res.render('weathermap', { temp, name, country, main, description, longitude, latitude, base, clouds })
 });
 
 app.get('/harrypotter', async (req, res) => {
@@ -51,7 +56,9 @@ app.get('/nasa', async (req, res) => {
     console.log(photoData);
     let copyright = photoData.copyright;
     let date = photoData.date;
-    res.render('nasa', { copyright, date });
+    let explanation = photoData.explanation;
+    let image = photoData.url;
+    res.render('nasa', { photoData: { copyright, date, explanation }, image});
     /*boolean: true*/
 });
 
@@ -63,12 +70,14 @@ app.get('/nasaasteroid', async (req, res) => {
 })
 
 app.get('/pokemon', async (req, res) => {
-    let pokeData = await openPokemon.getPokemon();
-    console.log(pokeData);
-    let name = pokeData.name;
-    let abilityOne = pokeData.abilities[0].ability.name;
-    let abilityTwo = pokeData.abilities[1].ability.name;
-    res.render('pokemon', { name, abilityOne, abilityTwo })
+    res.render('pokemon');
+})
+
+app.post('/pokemon', async (req, res) => {
+    let id = req.body.id
+    let response = await Pokemon(id);
+    console.log(response);
+    res.render('pokemon', { response });
 })
 
 app.get('/superhero', async (req, res) => {
@@ -81,20 +90,27 @@ app.get('/superhero', async (req, res) => {
 
 app.post('/superhero', async (req, res) => {
     let number = req.body.number
-    let powerData = await openSuperHero.getCharacterPowerStats(number);
+    let response = await SuperHero(number);
+    console.log(response);
     //let response  = await openSuperHero.getCharacterPowerStats(number);
-    let name = powerData.name;
-    let intelligence = powerData.intelligence;
-    res.render('superhero', {
-         powerData: {
-             name, intelligence
-        } 
-    })
+    //let name = powerData.name;
+    //let intelligence = powerData.intelligence;
+    res.render('superhero', { response });
 })
+
 app.listen(3000, () => {
-    console.log('listen to port');
+    console.log(`listen to 3000`);
 
 })
+
+
+
+
+
+
+
+
+
 /*app.get('/page2', (req, res) => {
     res.send('Yooooo again');
 
